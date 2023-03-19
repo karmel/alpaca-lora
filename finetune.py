9537bc3d04bf2eb71394c29fcf297535c162744e
Karmel Allison <karmel@arcaio.com>
2023-03-19 08:50:29 -0700

    - Reverting

--- finetune.py
+++ finetune.py
@@ -18,7 +18,7 @@ from peft import (
 
 # optimized for RTX 4090. for larger GPUs, increase some of these?
 MICRO_BATCH_SIZE = 4  # this could actually be 5 but i like powers of 2
-BATCH_SIZE = 32
+BATCH_SIZE = 128
 GRADIENT_ACCUMULATION_STEPS = BATCH_SIZE // MICRO_BATCH_SIZE
 EPOCHS = 3  # we don't need 3 tbh
 LEARNING_RATE = 3e-4  # the Karpathy constant
